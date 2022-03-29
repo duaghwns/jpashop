7243b776abc269b8176cbdd62944a22f4f7f8168
duaghwns <hj2758@gmail.com>
2022-03-30 00:45:19 +0900

    Index page

--- src/main/java/jpabook/jpashop/controller/IndexController.java
+++ src/main/java/jpabook/jpashop/controller/IndexController.java
@@ -1,13 +1,15 @@
 package jpabook.jpashop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("data", "hojoon");
         return "index";
     }
 
