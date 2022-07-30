af6c5c1ecebf72f4c3394735aa25b83026e2006f
duaghwns <hj2758@gmail.com>
2022-07-30 13:24:42 +0900

    indexcon

--- src/main/java/jpabook/jpashop/controller/IndexController.java
+++ src/main/java/jpabook/jpashop/controller/IndexController.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -16,5 +17,11 @@ public class IndexController {
     @GetMapping("login")
     public String login(){
         return "login";
+    }
+
+    @PostMapping("")
+    public String loginPost() {
+
+        return "/";
     }
 }
